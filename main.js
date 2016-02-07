860e2482cd03636244a35c1db71326224a46fba4
Brian Duimstra <brianduimstra@Brians-iMac.local>
2016-02-07 01:10:08 -0800

    first commit
    adding index file and main css/less and js

---
+++ main.js
@@ -0,0 +1,12 @@
+/*global $, jQuery*/
+
+$(function ($) {
+    'use strict';
+
+    function toggleNav() {
+
+    }
+
+    // event listeners/triggers
+    $('nav > span').on('click', toggleNav);
+})(jQuery);
